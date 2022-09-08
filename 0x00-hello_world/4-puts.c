cbf339de9ea03959f8af3dc706851d7c8669279a
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2022-09-08 12:20:45 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Function: prints string
+ * with puts function
+ * followed by a newline
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+/* declare the string to print as char[] */
+char string[] = "\"Programming is like building a multilingual puzzle";
+puts(string);
+return (0);
+}
