49297655ebf3bdaf143690380991203547ac3ab0
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2022-09-09 08:54:17 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Function: This function
+ * prints the english
+ * alphabets in lower case
+ * using the putchar function
+ * ending with a newline
+ *
+ * Return: ALways 0
+ *
+ */
+
+int main(void)
+{
+
+	int n = 0;
+	char c = 'a';
+
+	for (n = 0; n < 26; n++)
+	{
+		if (c + n != 'q' && c + n != 'e')
+			putchar(c + n);
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -2,3 +2,4 @@
 1-last_digit.c: This program generates a random numebr and describes its last digit
 2-print_alphabet.c: This program prints the lowercase of the english alphabet followed by a newline
 3-print_alphabets.c: This program prints the alphabet in lowercase, and then in uppercase, followed by a new line.
+4-print_alphabt.c: This program prints the english alphabet in lowercase except q and e
