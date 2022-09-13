26b46e645ba3376b4948ccea36c8c6774d030fbe
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2022-09-13 10:03:52 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Function: This function
+ * prints the word _puchar
+ * followed by a newline
+ *
+ * Return: ALways 0
+ *
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 0-putchar.c: This program prints _putchar followed by a newline
+1-alphabet.c: This program prints the alphabets in lower case
 
