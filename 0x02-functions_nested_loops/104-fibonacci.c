5c884ec912a6c416514b91073cdac8a2b99d793e
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2022-09-14 16:22:22 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -21,11 +21,11 @@ unsigned long int next;
 printf("%lu, ", n1);
 printf("%lu, ", n2);
 
-for (n = 2; n < 97; n++)
+for (n = 2; n < 98; n++)
 {
 
 	next = n2 + n1;
-	if (n == 96)
+	if (n == 9)
 		printf("%lu\n", next);
 	else
 		printf("%lu, ", next);
