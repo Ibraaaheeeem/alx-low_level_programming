25cc89c05bb8f7461bfe8765d9473cd18e424ef8
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2022-09-14 15:51:38 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * This program prints the first 50 Fibonacci numbers
+ * starting with 1 and 2, followed by a new line
+ *
+ * main - entry point
+ *
+ * Functions: prints the first 50 Fibonacci numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int n = 0;
+long int n1 = 1;
+long int n2 = 2;
+long int next;
+long long int sum = 2;
+
+for (n = 0; ; n++)
+{
+	next = n2 + n1;
+	if (next > 4000000)
+	{
+		printf("%lli\n", sum);
+		break;
+	}
+	if (next % 2 == 0)
+		sum += next;
+	n1 = n2;
+	n2 = next;
+}
+return (0);
+}
