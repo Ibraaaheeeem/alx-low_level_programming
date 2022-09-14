efbacf426f06317318a0dc9c8d404239794544cc
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2022-09-13 19:23:16 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * times_table - function name
+ *
+ * Function: This function
+ * prints the 9 times table
+ * starting with 0
+ *
+ * Return: Nothing
+ *
+ */
+
+void times_table(void)
+{
+	int n1 = 0, n2 = 0;
+
+	for (n1 = 0; n1 < 9; n1++)
+	{
+		for (n2 = 0; n2 < 60; n2++)
+		{
+			p = n1 * n2;
+			
+			if (p > 10)
+			{
+				_putchar(p / 10 + '0');
+				_putchar(p % 10 + '0');
+				if (n2 < 8)
+					_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(p + '0');
+				if (n2 < 8>
+					_putchar(',');
+				_putchar(' ');
+				_putchar(' ');	
+			}
+		}
+		_putchar('\n');
+
+	}
+}
+
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -7,3 +7,4 @@
 6-abs.c: This program prints the absolute value of an integer
 7-print_last_digit.c: This program prints the last digit of a number
 8-24_hours.c: This program prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+9-times_table.c: This program prints the 9 times table starting with 0
