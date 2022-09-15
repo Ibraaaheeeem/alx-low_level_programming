f7ff14b46e81943cd2517a3a25696afd2a41288d
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2022-09-14 17:42:48 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,24 +13,43 @@
  */
 int main(void)
 {
-int n;
+
+unsigned long int i;
 unsigned long int n1 = 1;
 unsigned long int n2 = 2;
-unsigned long int next;
+unsigned long int l = 1000000000;
+unsigned long int l1a;
+unsigned long int l1b;
+unsigned long int l2a;
+unsigned long int l2b;
 
-printf("%lu, ", n1);
-printf("%lu, ", n2);
+printf("%lu", n1);
+printf(", %lu", n2);
 
-for (n = 2; n < 100; n++)
+for (i = 2; i < 91; i++)
 {
-
-	next = n2 + n1;
-	if (n == 99)
-		printf("%lu\n", next);
-	else
-		printf("%lu, ", next);
-	n1 = n2;
-	n2 = next;
+	printf(", %lu", n2);
+	n2 += n1;
+	n1 = n2 - n1;
 }
+
+l1a = (n1 / l);
+l1b = (n1 % l);
+l2a = (n2 / l);
+l2b = (n2 % l);
+
+
+for (i = 92; i < 99; ++i)
+{
+printf(", %lu", l2a + (l2b / l));
+printf("%lu", l2b % l);
+l2a = l2a + l1a;
+l1a = l2a - l1a;
+l2b = l2b + l1b;
+l1b = l2b - l1b;
+
+}
+
+printf("\n");
 return (0);
 }
