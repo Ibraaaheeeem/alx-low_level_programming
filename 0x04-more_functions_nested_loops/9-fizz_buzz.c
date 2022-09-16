bad77969e19de9ba1444eb1306812176e99db100
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2022-09-15 21:04:44 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - Entry point
+ *
+ * Function: This function
+ * prints numbers 1 - 100
+ * with Fizz Buzz
+ * followed by a newline
+ *
+ *
+ */
+
+int main()
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		
+		if (n % 15 == 0)
+			printf("%s ", "FizzBuzz");
+		else if (n % 3 == 0)
+			printf("%s ", "Fizz");
+		else if (n % 5 == 0)
+			printf("%s ", "Buzz");
+		else if(n < 100)
+			printf("%d ", n);
+		else if (n == 100)
+			printf("%d\n", n);
+	}
+}
