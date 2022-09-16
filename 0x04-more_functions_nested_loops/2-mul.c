1c9e822246ee3a618007d451ba9f4692b8fce47d
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2022-09-15 19:10:01 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * mul- Entry point
+ *
+ * Function: This function
+ * multiplies two integers
+ *
+ * @a : This is the first number
+ *
+ * @b : This is the second number
+ *
+ * Return: The product of multiplication
+ *
+ */
+
+int mul(int a, int b)_
+{
+	product = a * b;
+	return (product);	
+}
