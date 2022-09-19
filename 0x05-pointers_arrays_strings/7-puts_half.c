0c6e1e56c7a8790610663ce438a54783741b7230
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2022-09-19 14:20:26 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * puts_half - This function
+ * prints half of a string to stdout
+ *
+ * @str: The string variable to print
+ *
+ */
+
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int j = 0;
+	int n;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if (i % 2 == 0)
+		n = i / 2;
+
+	else if (i % 2 == 1)
+		n = (i - 1) / 2
+
+	for (j = n; j < i; j++)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
