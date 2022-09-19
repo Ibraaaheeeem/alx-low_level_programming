9147849d7bd253d81c0764540e7dd4d207de7ff4
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2022-09-19 13:25:54 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts2 - This function
+ * prints a string to stdout
+ * at every other character 
+ *
+ * @str: The string variable to print
+ *
+ */
+
+void puts2(char *str);
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(s[i]);
+		i++;
+	}
+	_putchar('\n');
+}
