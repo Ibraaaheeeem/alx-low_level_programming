93d69dce5d88e346aab5f05888a5fe89edf07c63
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2022-09-19 10:55:43 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+/**
+ * _puts - This function
+ * prints a string to stdout
+ *
+ * @str: The string variable to print
+ *
+ */
+
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i];
+		i++;
+	}
+}
