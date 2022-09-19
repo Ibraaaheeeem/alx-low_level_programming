8fb0fea7db3cfa818c1d4c2353575130fc0d847a
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2022-09-19 11:30:11 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,13 +12,13 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (str[i] != '\0')
+	while (s[i] != '\0')
 	{
 		i++;
 	}
 	while (i != 1)
 	{
-	_putchar(str[i - 1]);
+	_putchar(s[i - 1]);
 	i--;
 	}
 	_putchar('\n');
