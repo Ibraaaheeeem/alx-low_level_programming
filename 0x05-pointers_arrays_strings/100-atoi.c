54588da15c2f3dae4b0e72c6195aca1a250b79b9
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2022-09-20 06:12:22 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -22,12 +22,8 @@ int _atoi(char *s)
 		if (s[i] >= 48 && s[i] <= 57)
 		{
 			n = n * 10 + s[i];
-			if (s[i + 1] < '0' || s[i + 1] > '9')
+			if (s[i + 1] < 48 || s[i + 1] > 57)
 				break;
-		}
-		else if (s[i] == '+')
-		{
-			plus++;
 		}
 		else if (s[i] == '-')
 		{
