1186173d66a248c4a1cee28dd94588a253208e92
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2022-09-20 07:07:37 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,21 +14,21 @@ int _atoi(char *s)
 {
 	int i = 0;
 	int n = 0;
+	int brk;
 	int sign = 1;
 
 	while (s[i] != '\0')
 	{
-
 		if (s[i] == '-')
 			sign = sign * -1;
-
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			n = n * 10 - (s[i] - '0');
-			if (s[i] < '0' || s[i] > '9')
-				break;
+			n = n * 10;
+			n -= (s[i] - '0');
+			brk = 1;
 		}
-		i++;
+		else if (brk == 1)
+			break;
 	}
 	return (n * sign);
 }
