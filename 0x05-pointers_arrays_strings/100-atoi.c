a5fbba9e7fc57a690cc4b46ac23e40536fdf1ad6
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2022-09-19 20:56:02 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi- This function
+ * converts a string to an integer
+ *
+ * @s: The string variable to print
+ *
+ */
+
+
+void _atoi(char *s)
+{
+	int i = 0;
+	int n_times = 0;
+	int n = 0;
+	int plus = 0;
+	int minus = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 48 && s[i] <= 57)
+		{
+			n = n * 10 + (s[i] + '0');
+		}
+		else if (s[i] == '+')
+		{
+			plus++;
+		}
+		else if (s[i] == '-')
+		{
+			minus++;
+		}
+
+		i++;
+	}
+	if (minus > plus)
+		n = -n;
+	printf("%d\n", n);
+}
