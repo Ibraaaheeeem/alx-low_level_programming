cc8bd3efbdcd646c32fe7bece66bfd601a18414c
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2022-09-21 20:00:05 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rot13- encodes a string with rot13
+ * all words of a string
+ * @s: string to convert
+ * Return: converted string
+ */
+
+char *rot13(char *s)
+{
+	int i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		s[i] = s[i] + 13;
+		if (s[i] > 'z')
+			s[i] -= 'z';
+	}
+
+	return (s);
+}
