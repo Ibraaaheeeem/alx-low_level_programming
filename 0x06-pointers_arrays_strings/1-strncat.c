25ca4ad5cccb349a78b60319d4aa6f4e38cc9972
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2022-09-21 18:20:28 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,28 +12,26 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0, j = 0;
+	int i = 0, j = 0, k = 0;
 
-	while (dest[i] != '\0')
+	for (i = 0; ; i++)
 	{
+		if (dest[i] == '\0')
+		{
+			break;
+		}
+		k++;
+	}
+
+	while (src[j] != '\0')
+	{
+		dest[k + i] = src[j];
+		j++;
 		i++;
+		if (i >= n)
+			break;
 	}
+	dest[k + i] = '\0';
 
-/*
-* while (src[j] != '\0')
-* {
-*dest[i] = src[j];
-*j++;
-*i++;
-*if (i >= n - 1)
-*break;
-*}
-*dest[i] = '\0';
-*/
-	for (i = 0; src[i] != '\0' && i < n; i++)
-	{
-		dest[m + i] = src[i];
-	}
-	dest[m + i] = '\0';
 	return (dest);
 }
