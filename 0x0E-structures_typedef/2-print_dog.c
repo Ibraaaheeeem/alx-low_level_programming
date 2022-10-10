b5070f29508241fd0cf4c42ea26a814cff6390fa
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2022-10-10 14:56:44 -0700

    Rewrite

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,18 +9,10 @@
 
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
-		return;
-	if (d->name)
-		printf("Name: %s\n", d->name);
-	else
-		printf("(nil)\n");
-	if (d->age)
-		printf("Age: %f\n", d->age);
-	else
-		printf("%d\n", 0);
-	if (d->owner)
-		printf("Owner: %s\n", d->owner);
-	else
-		printf("(nil)\n");
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+		printf("Age: %f\n", (d->age) ? d->age : 0);
+		printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
+	}
 }
