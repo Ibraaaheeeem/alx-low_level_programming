ca471b2787341d61948e22f29677dab7c9cf392e
Ibraaaheeeem <ibraaaheeeem@gmail.com>
2022-10-27 10:40:04 -0700

    Correct Convert Binary to int in C

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdlib.h>
+#include <string.h>
 
 /**
  * binary_to_uint - this program converts a
@@ -11,9 +11,9 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int count, n = 0, base = 0;
-	int power, num;
+	unsigned int num;
 
-	count = strlen(b);
+	count = _strlen(b);
 	num = 0;
 	if (!b)
 	{
@@ -21,11 +21,11 @@ unsigned int binary_to_uint(const char *b)
 	}
 	for (n = count - 1; n >= 0; n--)
 	{
-		if (!(*b == '0' || *b == '1'))
+		if ((b[n] < '0' || b[n] > '1'))
 		{
 			return (0);
 		}
-		if (*b == '1')
+		else if (b[n] == '1')
 		{
 			num += num + base;
 		}
@@ -33,3 +33,21 @@ unsigned int binary_to_uint(const char *b)
 	}
 	return (num);
 }
+/**
+ * _strlen - gets the lenght of input strings
+ * @str: input string
+ * Return: len of the string
+ */
+
+int _strlen(const char *str)
+{
+	int len = 0;
+
+	while (*str)
+	{
+		len++;
+		str++;
+	}
+	return (len);
+}
+
